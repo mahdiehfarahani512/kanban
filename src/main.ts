dfccb88eda1c75a4f2605860f0df7702e4bca4f3
mahdieh <mahdiefarahani00@gmail.com>
2025-10-18 11:07:41 +0330

    v6

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
 import "./style.css";
+
+const creatEl=document.getElementById("creatTask") as HTMLDivElement
+const addEl=document.getElementById("addTask") as HTMLButtonElement
+
+
+addEl.addEventListener("click",()=>{
+ creatEl.classList.remove("hidden")
+})
+
+
+
+
